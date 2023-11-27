078b809bf751511a84ad938eee597517bec3410f
Priscila Herculano <priscilarhcorreia@gmail.com>
2023-11-27 17:58:37 -0300

    init

--- Atividade54/Program.cs
+++ Atividade54/Program.cs
@@ -1,2 +1,25 @@
 ﻿//Estrutura repetitiva "para"
-//Fazer um programa que lê um valor inteiro N e depois N números inteiros. Ao final, mostra a soma dos números lidos
+//Fazer um programa que lê um valor inteiro N e depois N números inteiros. Ao final, mostra a soma dos números lidos.
+
+using System;
+
+namespace Atividade54{
+
+    class Program{
+
+        static void Main(string [] args){
+
+            int N, x, soma;
+
+            N = int.Parse(Console.ReadLine());
+            
+            soma = 0;
+            for (int i=10; i>=N; i--){
+                Console.WriteLine("valor: " + i);
+                // x = int.Parse(Console.ReadLine());
+                // soma = soma + x; 
+            }
+            // Console.WriteLine(soma);
+        }
+    }
+}
---
+++ Atividade55/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+    namespace Atividade55{
+        class Program{
+            static void Main(string[]args){
+
+                int i, soma;
+                int N = int.Parse(Console.ReadLine());
+
+                for(i = 0; i < 11; i++){
+                    soma = N * i;
+                    Console.WriteLine(i + " X " + N + "=" + soma);
+                }
+            }
+
+        }
+    }
